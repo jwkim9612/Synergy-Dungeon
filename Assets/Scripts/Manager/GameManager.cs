f6767016167e59bb514f3222a91db63d872ff0f5
김건우 <kkwoo94517@gamil.com>
2020-04-04 11:15:36 +0900

    [김건우] 기본 매니저 추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //매니저들
+    public static GameManager instance = null;
+    public UIManager uiManager = null;
+
+    //파괴되지 않는 싱글턴
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        // uiManager.Initialize();
+    }
+}
---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private bool isInitialized = false;
+
+    //UI 기록
+    private Stack<UIControl> uiHistory = new Stack<UIControl>();
+
+    {
+        //매니저 등록
+        GameManager.instance.uiManager = this;
+    }
+
+    void Start()
+    {
+        if(isInitialized)
+        {
+            return;
+        }
+
+        isInitialized = true;
+        Initialize();
+    }
+
+    public void ShowMessage(string _message)
+    {
+        messageUI.ShowMessage(_message);
+    }
+
+    void Update()
+    {
+        //Back키 입력 시 뒤로 가기
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HideAndShowPreview();
+        }
+    }
+
+    //새로운 UIControl 추가 및 표시
+    public void ShowNew(UIControl newUI)
+    {
+        newUI.OnShow();
+        uiHistory.Push(newUI);
+    }
+
+    public void AddHistroy(UIControl newUI)
+    {
+        uiHistory.Push(newUI);
+    }
+
+    public void DeleteHistory()
+    {
+        uiHistory.Pop();
+    }
+
+    //이전 UIControl 숨김 후 새로운 UIControl 추가 및 표시
+    public void HidePreviewAndShowNew(UIControl newUI)
+    {
+        if (uiHistory.Count != 0)
+        {
+            uiHistory.Peek().OnHide();
+        }
+        newUI.OnShow();
+        uiHistory.Push(newUI);
+    }
+
+    //현재 UIControl 숨김 후 이전 UIControl 표시
+    public void HideAndShowPreview()
+    {
+        if (uiHistory.Count != 0)
+        {
+            uiHistory.Pop().OnHide();
+
+            if (uiHistory.Count != 0)
+            {
+                uiHistory.Peek().OnShow();
+            }
+        }
+        else
+        {
+            //아무 UI도 표시 안되있을 경우 종료 UI 표시
+            ShowNew(exitUIControl);
+        }
+    }
+}
