160cd902c81b6cd11a9b483237acba06d5864939
jwkim9612 <jwkim9612@gmail.com>
2020-04-05 15:42:12 +0900

    [김정우] 메인화면 틀을 만듬

---
+++ Assets/Scripts/UI/MainMenu/UIMainExit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainExit : UIControl
+{
+    [SerializeField] private Button exitButton;
+
+    void Start()
+    {
+        exitButton.onClick.AddListener(() => {
+            GameManager.instance.Quit();
+        });
+    }
+}
