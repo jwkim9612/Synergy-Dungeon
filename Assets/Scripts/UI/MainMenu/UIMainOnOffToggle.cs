ab0a367a5221e4a3cf94c1df98c8999cbb6c420b
jukim9612 <52120400+jwkim9612@users.noreply.github.com>
2020-04-08 15:33:18 +0900

    [김정우] 메인화면과 옵션의 UI 수정

---
+++ Assets/Scripts/UI/MainMenu/UIMainOnOffToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainOnOffToggle : MonoBehaviour
+{
+    public GameObject clickedImage;
+    public GameObject unclickedImage;
+    
+    public void OnChangeValue()
+    {
+        bool onoffSwitch = gameObject.GetComponent<Toggle>().isOn;
+        if(onoffSwitch)
+        {
+            clickedImage.SetActive(true);
+            unclickedImage.SetActive(false);
+        }
+        else
+        {
+            clickedImage.SetActive(false);
+            unclickedImage.SetActive(true);
+        }
+
+    }
+
+    void Start()
+    {
+        OnChangeValue();
+    }
+}
