5f0bb521297c1ae6f8927cb9a4542fc36aec970e
jwkim9612 <jwkim9612@gmail.com>
2020-04-21 22:22:13 +0900

    [김정우] Shared 재 업로드

---
+++ Assets/Shared/Shared/Service/RandomService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Service
+{
+    public class RandomService
+    {
+        //public const int NUM_Of_FRACTION = 10000;
+        public static System.Random random = new System.Random();
+
+        public static float GetRandom()
+        {
+            float randomNum = (float)random.NextDouble();
+
+            return randomNum;
+        }
+    }
+}
