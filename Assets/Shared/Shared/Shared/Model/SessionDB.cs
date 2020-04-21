13de123bfdf00a50e819a233434316dacdaf981b
김건우 <kkwoo94517@gamil.com>
2020-04-21 20:52:26 +0900

    [김건우] Shared 초기 업로드

---
+++ Assets/Shared/Shared/Shared/Model/SessionDB.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Shared.Model
+{
+    public class SessionKey
+    {
+        public long SessionServerId { get; set; }
+        public long AccountServerId { get; set; }
+
+        [JsonIgnore]
+        public bool HasValue => SessionServerId != 0 && AccountServerId != 0;
+    }
+
+    public class SessionDB
+    {
+        public SessionKey SessionKey { get; set; }
+        public DateTime LastConnect { get; set; }
+        public int ConnectionTime { get; set; }
+    }
+}
---
+++ Assets/Shared/Shared/Shared/NetworkProtocol/BaseProtocol.cs
@@ -0,0 +1,56 @@
+﻿using DBModel;
+using System;
+
+namespace Shared.NetworkProtocol
+{
+    public enum Protocol
+    {
+        #region -9999 ~ 999 : System Protocol
+        Common_Cheat = -9999,
+        Error = -1,
+        None = 0,
+        System_Version,
+        Session_Info,
+        #endregion
+    }
+
+    public enum WebAPIErrorCode
+    {
+        #region system error code
+        None,
+        InvalidPacket,
+        InvalidProtocol,
+        InvalidSession,
+        InvalidVersion,
+        InternalServerError,
+        DBError,
+        #endregion
+    }
+
+    public abstract class BasePacket
+    {
+        /// <summary> Key </summary>
+        public SessionKey SessionKey { get; set; }
+
+        public abstract Protocol Protocol { get; }
+    }
+
+    public abstract class RequestPacket : BasePacket
+    {
+        public int ClientUpTime { get; set; }
+    }
+
+    public abstract class ResponsePacket : BasePacket
+    {
+        public int ServerTimestamp { get; set; }
+    }
+
+    public class ErrorPacket : ResponsePacket
+    {
+        public override Protocol Protocol => Protocol.Error;
+
+        public string Reason { get; set; }
+
+        public WebAPIErrorCode ErrorCode { get; set; }
+    }
+}
