76f8f331eb3a2a394bbff81aa225034c6a8a962e
김건우 <kkwoo94517@gamil.com>
2020-05-14 23:14:44 +0900

    [김건우] GameSparks SDK 업로드

---
+++ Assets/Scripts/UI/StartScene/UISignGuest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISignGuest : MonoBehaviour
+{
+    [SerializeField] private InputField idInputField;
+    [SerializeField] private InputField pwInputField;
+
+    public void OnClick_GuestSign()
+    {
+        Debug.Log(idInputField.text.ToString());
+        Debug.Log(pwInputField.text.ToString());
+        new GameSparks.Api.Requests.RegistrationRequest()
+            //.SetDisplayName(displayNameInput.text) // DisplayName 이 닉네임일듯
+            .SetUserName(idInputField.text) // 계정아이디
+            .SetPassword(pwInputField.text) // 비밀번호
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log("회원가입 완료");
+                }
+                else
+                {
+
+                    Debug.Log("회원가입 실패" + response.Errors.JSON.ToString());
+                }
+            }
+        );
+    }
+}
