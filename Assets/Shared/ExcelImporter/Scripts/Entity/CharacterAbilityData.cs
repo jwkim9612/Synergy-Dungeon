cae37508276730493c260cad7223111838ec000f
jukim9612 <52120400+jwkim9612@users.noreply.github.com>
2020-05-26 19:34:36 +0900

    [김정우] 수정한 데이터 시트

--- Assets/Shared/ExcelImporter/Scripts/Entity/CharacterAbilityData.cs
+++ Assets/Shared/ExcelImporter/Scripts/Entity/CharacterAbilityData.cs
@@ -9,12 +9,13 @@ public class CharacterAbilityData
     public long Id;
     public string Name;
     public long Attack;
-    public long MagicAttack;
+    public long MagicalAttack;
     public long Health;
     public long Defence;
     public long MagicDefence;
     public long Shield;
     public long Accuracy;
     public long Evasion;
+    public long Critical;
     public long AttackSpeed;
 }
--- Assets/Shared/ExcelImporter/Scripts/Entity/EnemyData.cs
+++ Assets/Shared/ExcelImporter/Scripts/Entity/EnemyData.cs
@@ -9,13 +9,14 @@ public class EnemyData
     public long Id;
     public string Name;
     public long Attack;
-    public long MagicAttack;
+    public long MagicalAttack;
     public long Health;
     public long Defence;
     public long MagicDefence;
     public long Shield;
     public long Accuracy;
     public long Evasion;
+    public long Critical;
     public long AttackSpeed;
     public string ImagePath;
 
